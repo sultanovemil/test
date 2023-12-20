dcefc3521a78ecd802142caf40ed86b61852c41a
Emil Sultanov <149473730+sultanovemil@users.noreply.github.com>
2023-12-21 01:00:52 +0500

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -1,10 +1,12 @@
 import os
 
 from streamlit.testing.v1 import AppTest
+secret_val = os.getenv('API_K')
 
-a = os.getenv('API_K')
-# Just a simple test
+secrets = {"API_K": os.getenv('API_K')}
+
 def test_url_input():
-    at = AppTest.from_file('main.py', default_timeout=30) 
+    at = AppTest.from_file('main.py', default_timeout=30, secrets=secrets) 
     at.run()
-    assert at.success[0].value == 'bad'
+    at.secrets['API_K'] 
+    assert at.success[0].value == 'good'
