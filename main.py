f3083fcbc5f24505e658696ef78141f2859f98f0
Emil Sultanov <149473730+sultanovemil@users.noreply.github.com>
2023-12-21 00:55:40 +0500

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import streamlit as st
 import os
 
 a = os.getenv('API_K')
-st.text_area(key='ta', value='hi')
 if a == 'hello':
-  st.success('ok')
-  st.text_area(key='ta', value='good')
+  st.success('good')  
+else: 
+  st.success('bad')
   
