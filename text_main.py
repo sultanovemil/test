c35f8c8a6b466a1b8a151423e72f9026a36bebb0
Emil Sultanov <149473730+sultanovemil@users.noreply.github.com>
2023-12-20 23:56:40 +0500

    Create text_main.py

---
+++ text_main.py
@@ -0,0 +1,7 @@
+import os
+from streamlit.testing.v1 import AppTest
+
+def test_url_input():
+    at = AppTest.from_file('main.py', default_timeout=30) 
+    at.run()    
+    assert at.success[0].value == 'ok'
