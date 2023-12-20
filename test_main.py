5221c5551742a7839827d21168ec6b543078bd02
Emil Sultanov <149473730+sultanovemil@users.noreply.github.com>
2023-12-21 00:53:31 +0500

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -7,4 +7,4 @@ a = os.getenv('API_K')
 def test_url_input():
     at = AppTest.from_file('main.py', default_timeout=30) 
     at.run()
-    assert st.success[0].value == 'ok'
+    assert at.success[0].value == 'ok'
