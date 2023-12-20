0fd3cc1f9d4d3e4a2db80a2f7d55c0135df7ce7c
Emil Sultanov <149473730+sultanovemil@users.noreply.github.com>
2023-12-21 00:29:44 +0500

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -5,3 +5,4 @@ def test_url_input():
     at = AppTest.from_file('main.py', default_timeout=30) 
     at.run()    
     assert at.success[0].value == 'ok'
+    assert at.text_area(key='ta').value == 'good'
