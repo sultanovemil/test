1c70857dbd58acee7cabffcb9b857076406ff8ad
Emil Sultanov <149473730+sultanovemil@users.noreply.github.com>
2023-12-21 09:05:33 +0500

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -2,8 +2,7 @@ import os
 
 from streamlit.testing.v1 import AppTest
 
-def test_fun():
-    at = AppTest.from_file('main.py', default_timeout=30)
-    at.secrets['API_K'] = os.getenv('API_K')
-    at.run()   
-    assert at.success[0].value == 'good'
+at = AppTest.from_file('main.py', default_timeout=30)
+at.secrets['API_K'] = os.getenv('API_K')
+at.run()   
+assert at.success[0].value == 'good'
