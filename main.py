d989fc4e397f8359a638b8c184799aff30ecbb62
Emil Sultanov <149473730+sultanovemil@users.noreply.github.com>
2023-12-21 08:48:40 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import streamlit as st
 import os
 
-#a = os.getenv('API_K')
-a = st.secrets['API_K']
+a = os.getenv('API_K')
+#a = st.secrets['API_K']
 if a == 'hello':
   st.success('good')  
 else: 
